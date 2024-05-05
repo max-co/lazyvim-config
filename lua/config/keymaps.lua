d0b5e5c833b52eb793bc0be0bd7e9e8f6dcbf780
Massimo Comuzzo <43844835+max-co@users.noreply.github.com>
2024-05-05 16:55:04 +0200

    Add dap for Go and basic keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<F5>", '<cmd>lua require("dap").continue()<CR>')
+vim.keymap.set("n", "<F10>", '<cmd>lua require("dap").step_over()<CR>')
+vim.keymap.set("n", "<F11>", '<cmd>lua require("dap").step_into()<CR>')
+vim.keymap.set("n", "<F12>", '<cmd>lua require("dap").step_out()<CR>')

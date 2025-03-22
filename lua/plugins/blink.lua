782940026cf1cc7b93a45d693d5369b74be4c654
Massimo Comuzzo <43844835+max-co@users.noreply.github.com>
2025-03-22 22:03:55 +0100

    Add completion using tab in blink

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,10 @@
+return {
+  "saghen/blink.cmp",
+  -- optional: provides snippets for the snippet source
+  dependencies = { "rafamadriz/friendly-snippets" },
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = { preset = "super-tab" },
+  },
+}

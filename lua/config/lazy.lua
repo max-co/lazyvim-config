4baacd7f000229409a27488385665ab3ac7be477
Massimo Comuzzo <43844835+max-co@users.noreply.github.com>
2025-04-05 15:45:00 +0200

    Enable SQL extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     -- import/override with your plugins
     { import = "plugins" },
